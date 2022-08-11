2b079ee4746d9deffb5b1cfbc0d2c6bb24c9caa8
felipecosta <felipesaev@gmail.com>
2022-08-11 08:32:16 -0300

    refactor: remove console.log

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import Link from 'next/link'
 import { Dropdown } from 'flowbite-react'
+import { Container } from '../Grid/Container/styles'
 
 type Game = {
   id: string
@@ -25,17 +26,18 @@ export type MapProps = {
 
 export const Map = ({ games }: MapProps) => (
   <>
-    <div style={{ width: '100%', height: '35vh' }}>
-      <Link href="/">
-        <button>Back</button>
-      </Link>
-      <br />
-      <br />
-      <div className="flex justify-start">
-        <div className="mb-3 xl:w-96">
-          <label>Escolha a cidade</label>
-          <select
-            className="form-select appearance-none
+    <Container>
+      <div style={{ width: '100%', height: '35vh' }}>
+        <Link href="/">
+          <button>Back</button>
+        </Link>
+        <br />
+        <br />
+        <div className="flex justify-start">
+          <div className="mb-3 xl:w-96">
+            <label>Escolha a cidade</label>
+            <select
+              className="form-select appearance-none
       block
       w-full
       px-3
@@ -50,19 +52,19 @@ export const Map = ({ games }: MapProps) => (
       ease-in-out
       m-0
       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            aria-label="Default select example"
-          >
-            <option selected>São José dos Campos</option>
-            <option value="1">Jacarei</option>
-            <option value="2">Taubate</option>
-            <option value="3">São Sebastião</option>
-          </select>
+              aria-label="Default select example"
+            >
+              <option selected>São José dos Campos</option>
+              <option value="1">Jacarei</option>
+              <option value="2">Taubate</option>
+              <option value="3">São Sebastião</option>
+            </select>
 
-          <br />
+            <br />
 
-          <label>Escolha ao bairro</label>
-          <select
-            className="form-select appearance-none
+            <label>Escolha ao bairro</label>
+            <select
+              className="form-select appearance-none
       block
       w-full
       px-3
@@ -77,16 +79,17 @@ export const Map = ({ games }: MapProps) => (
       ease-in-out
       m-0
       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            aria-label="Default select example"
-          >
-            <option selected>São José dos Campos</option>
-            <option value="1">Jacarei</option>
-            <option value="2">Taubate</option>
-            <option value="3">São Sebastião</option>
-          </select>
+              aria-label="Default select example"
+            >
+              <option selected>São José dos Campos</option>
+              <option value="1">Jacarei</option>
+              <option value="2">Taubate</option>
+              <option value="3">São Sebastião</option>
+            </select>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
     <MapContainer
       center={[-23.230482, -45.8938317]}
       zoom={15}
@@ -98,8 +101,6 @@ export const Map = ({ games }: MapProps) => (
       />
       {games?.map(
         ({ id, title, addressLarge, situation, time, address, genre }) => {
-          console.log(situation)
-
           const { latitude, longitude } = addressLarge
           const { coverage, net, floor } = situation
 
