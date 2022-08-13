6b81c1c51e70c13e80052c3b7f5a12e76a65f6cb
felipecosta <felipesaev@gmail.com>
2022-08-13 11:57:02 -0300

    chore: add modalDetails

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,6 +2,8 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import Link from 'next/link'
 import { Dropdown } from 'flowbite-react'
 import { Container } from '../Grid/Container/styles'
+import { useState } from 'react'
+import { ModalDetail } from 'components/ModalDatail'
 
 type Game = {
   id: string
@@ -24,20 +26,35 @@ export type MapProps = {
   games?: Game[]
 }
 
-export const Map = ({ games }: MapProps) => (
-  <>
-    <Container>
-      <div style={{ width: '100%', height: '35vh' }}>
-        <Link href="/">
-          <button>Back</button>
-        </Link>
-        <br />
-        <br />
-        <div className="flex justify-start">
-          <div className="mb-3 xl:w-96">
-            <label>Escolha a cidade</label>
-            <select
-              className="form-select appearance-none
+function transitionPage() {
+  console.log('teste')
+
+  return <Link href="/game">dsa</Link>
+}
+
+export const Map = ({ games }: MapProps) => {
+  const [open, setOpen] = useState(false)
+
+  function close() {
+    setOpen(false)
+  }
+  function showModal(e) {
+    setOpen(!open)
+  }
+  return (
+    <>
+      <Container>
+        <div style={{ width: '100%', height: '35vh' }}>
+          <Link href="/">
+            <button className="mt-2">Back</button>
+          </Link>
+          <br />
+          <br />
+          <div className="flex justify-start">
+            <div className="mb-3 xl:w-96">
+              <label>Escolha a cidade</label>
+              <select
+                className="form-select appearance-none
       block
       w-full
       px-3
@@ -52,19 +69,19 @@ export const Map = ({ games }: MapProps) => (
       ease-in-out
       m-0
       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              aria-label="Default select example"
-            >
-              <option selected>São José dos Campos</option>
-              <option value="1">Jacarei</option>
-              <option value="2">Taubate</option>
-              <option value="3">São Sebastião</option>
-            </select>
+                aria-label="Default select example"
+              >
+                <option selected>São José dos Campos</option>
+                <option value="1">Jacarei</option>
+                <option value="2">Taubate</option>
+                <option value="3">São Sebastião</option>
+              </select>
 
-            <br />
+              <br />
 
-            <label>Escolha ao bairro</label>
-            <select
-              className="form-select appearance-none
+              <label>Escolha ao bairro</label>
+              <select
+                className="form-select appearance-none
       block
       w-full
       px-3
@@ -79,59 +96,55 @@ export const Map = ({ games }: MapProps) => (
       ease-in-out
       m-0
       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              aria-label="Default select example"
-            >
-              <option selected>São José dos Campos</option>
-              <option value="1">Jacarei</option>
-              <option value="2">Taubate</option>
-              <option value="3">São Sebastião</option>
-            </select>
+                aria-label="Default select example"
+              >
+                <option selected>São José dos Campos</option>
+                <option value="1">Jacarei</option>
+                <option value="2">Taubate</option>
+                <option value="3">São Sebastião</option>
+              </select>
+            </div>
           </div>
         </div>
-      </div>
-    </Container>
-    <MapContainer
-      center={[-23.230482, -45.8938317]}
-      zoom={15}
-      style={{ height: '100%', width: '100%', position: 'absolute' }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {games?.map(
-        ({ id, title, addressLarge, situation, time, address, genre }) => {
-          const { latitude, longitude } = addressLarge
-          const { coverage, net, floor } = situation
+      </Container>
+      <MapContainer
+        center={[-23.230482, -45.8938317]}
+        zoom={15}
+        style={{ height: '100%', width: '100%', position: 'absolute' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {games?.map(
+          ({ id, title, addressLarge, situation, time, address, genre }) => {
+            const { latitude, longitude } = addressLarge
+            const { coverage, net, floor } = situation
 
-          return (
-            <Marker
-              key={`place-${id}`}
-              position={[latitude, longitude]}
-              title={title}
-            >
-              <Popup>
-                <span>{title}</span>
-                <br />
-                <span>{time}</span>
-                <br />
-
-                <span>{address}</span>
-                <br />
-                <span>{genre}</span>
-                <br />
-                <span>{coverage}</span>
-                <br />
-                <span>{floor}</span>
-                <br />
-                <span>{net}</span>
-              </Popup>
-            </Marker>
-          )
-        }
-      )}
-    </MapContainer>
-  </>
-)
+            return (
+              <Marker
+                key={`place-${id}`}
+                position={[latitude, longitude]}
+                title={title}
+                eventHandlers={{
+                  click: (e) => {
+                    showModal(e)
+                  }
+                }}
+              >
+                {open && (
+                  <ModalDetail
+                    onBlur={close}
+                    style={{ display: open ? 'none' : 'block' }}
+                  />
+                )}
+              </Marker>
+            )
+          }
+        )}
+      </MapContainer>
+    </>
+  )
+}
 
 export default Map
---
+++ src/components/ModalDatail/index.tsx
@@ -0,0 +1,19 @@
+export const ModalDetail = () => {
+  return (
+    <div id="myModal" className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <span className="close">&times;</span>
+          <h2>Modal Header</h2>
+        </div>
+        <div className="modal-body">
+          <p>Some text in the Modal Body</p>
+          <p>Some other text...</p>
+        </div>
+        <div className="modal-footer">
+          <h3>Modal Footer</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
